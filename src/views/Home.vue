c0d27a34e1eeb701fd47c4c33c7f9a00f24cfd4d
changlin <767425412@qq.com>
2020-11-17 21:47:10 +0800

    feat:初始化完成

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>home</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'home',
+    setup() {
+        return {};
+    },
+});
+</script>
+
+<style scoped></style>
